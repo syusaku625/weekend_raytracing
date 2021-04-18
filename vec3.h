fbde8d27601b3b9c8d6eca137cd8d7b9a0897239
syusaku625 <maedasyu625@gmail.com>
2021-04-18 18:20:41 +0900

    update

--- vec3.h
+++ vec3.h
@@ -15,7 +15,7 @@ public:
     double y() const {return e[1];}
     double z() const {return e[2];}
 
-    vec3 operator-() const {return vec3(-e[0], -e[1], -e[2])}
+    vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]);}
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) {return e[i];}
 
@@ -46,7 +46,7 @@ public:
     }
 public:
     double e[3];
-}
+};
 
 using point3 = vec3;
 using color=vec3;
@@ -62,7 +62,7 @@ inline vec3 operator+(const vec3 &u, const vec3 &v){
 }
 
 inline vec3 operator-(const vec3 &u, const vec3 &v){
-    return vec3(u.e[0]-v.e[0], u.e[1]-v.e[1], u.e[2]-v.e[2])
+    return vec3(u.e[0]-v.e[0], u.e[1]-v.e[1], u.e[2]-v.e[2]);
 }
 
 inline vec3 operator*(const vec3 &u, const vec3 &v){
@@ -70,7 +70,7 @@ inline vec3 operator*(const vec3 &u, const vec3 &v){
 }
 
 inline vec3 operator*(double t, const vec3 &v){
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2])
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline vec3 operator*(const vec3 &v, double t){
@@ -81,8 +81,8 @@ inline vec3 operator/(vec3 v, double t){
     return (1/t)*v;
 }
 
-inline double dot(const vec3 &u, cnost vec3 &v){
-    return u.e[0]*v.e[0]+u.e[1]*v.e[1]+u.e[2]*v.e[2]
+inline double dot(const vec3 &u, const vec3 &v){
+    return u.e[0]*v.e[0]+u.e[1]*v.e[1]+u.e[2]*v.e[2];
 }
 
 inline vec3 cross(const vec3 &u, const vec3 &v){
