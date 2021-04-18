7da02d7f917d1a503006a92e8e453366e202b319
syusaku625 <maedasyu625@gmail.com>
2021-04-18 23:18:30 +0900

    22pageまで

--- hittable.h
+++ hittable.h
@@ -6,6 +6,11 @@
 struct hit_record {
     point3 p;
     vec3 normal;
+    bool front_face;
+    inline void set_face_normal(const ray& r, const vec3& outward_normal){
+        front_face = dot(r.direction(), outward_normal)<0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
     double t;
 };
 
